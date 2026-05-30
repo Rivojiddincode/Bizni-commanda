f788bd506f9dfe83221bac1bd22e40de5ad7bf52
IslomN2011 <islomnarzulaev069@gmail.com>
2026-05-30 15:34:54 +0500

    new update

--- src/components/islom/islom.jsx
+++ src/components/islom/islom.jsx
@@ -2,8 +2,11 @@
 
 const islom = () => {
   return (
-    <div>I am here
-        
+    <div>
+        <h1>i am here </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores minima omnis sequi? Velit reprehenderit id assumenda reiciendis ipsam consectetur exercitationem repellendus dolore beatae officiis error cupiditate impedit, magni amet ducimus.
+        </p>
+
     </div>
   )
 }
