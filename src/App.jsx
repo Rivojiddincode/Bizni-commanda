e5578dd7f6f35d319c6532207b9e6e3319271421
Shaxriyor1227 <kiyotasakiayanakoji@gmail.com>
2026-06-02 13:59:04 +0500

    NewProduct

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Fullproduct from './components/Productdetailpage/Fullproduct/Fullproduct'
+
+const App = () => {
+  return (
+    <div>
+        <Fullproduct />
+    </div>
+  )
+}
+
+export default App
